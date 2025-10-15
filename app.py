c6830374ddef4ec9c2354d7578fa77024f09c151
Victoria Izquierdo <a385983@uach.mx>
2025-10-15 08:07:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,35 +4,59 @@ import math
 st.title("Mi Aplicación para Calcular Figuras y Relaciones Trigonométricas en Streamlit 🖩")
 
 # Selección de figura
-figura = st.selectbox("Selecciona una figura geométrica",["Círculo"])
+figura = st.selectbox("Selecciona una figura geometrica", ["Círculo", "Triángulo", "Rectángulo", "Cuadrado"])
 
-# Widget para ingresar el radio
-radio = st.slider("Selecciona el radio", 0.0, 10.0, 5.0)
-
-# Cálculo de área
-area = math.pi * radio**2
-
-# Cálculo del perimetro
-perimetro = 2 * math.pi * radio
-
-# Mostrar resultado
-st.write(f"El área del círculo con radio {radio} es:{area:2f}")
-st.metric("Perímetro", f"{perimetro:.2f}")
-
-# Selección de figura
-figura = st.selectbox("Selecciona una figura geométrica",["Triángulo"])
-
-# Widget para ingresar el area
-area = st.slider("Seleccionar el area", 0.0, 10.0, 5.0)
-
-if figura == "Triangulo":
-    base = st.slider("Selecciona la base", 0.0, 10.0, 5.0)
-    altura = st.slider("Selecciona la altura", 0.0, 10.0, 5.0)
-    lado_a = st.slider("Lado a", 0.0, 10.0, 5.0)
-    lado_b = st.slider("Lado b", 0.0, 10.0, 5.0)
-    lado_c = st.slider("Lado c", 0.0, 10.0, 5.0)
-    area = 0.5 * base * altura
-    perimetro = lado_a + lado_b + lado_c
+# Círculo
+if figura == "Círculo":
+    radio = st.slider("Selecciona el radio", 0.0, 20.0, 5.0)
+# calculo del area
+    area = math.pi * radio**2
+#calculo del perimetro
+    perimetro = 2 * math.pi * radio
+# resultados
     st.metric("Área", f"{area:.2f}")
     st.metric("Perímetro", f"{perimetro:.2f}")
     st.success("¡Resultados!")
+
+# Triángulo
+elif figura == "Triángulo":
+    base = st.slider("Selecciona la base", 0.0, 20.0, 5.0)
+    altura = st.slider("Selecciona la altura", 0.0, 20.0, 5.0)
+    lado_a = st.slider("Lado a", 0.0, 20.0, 5.0)
+    lado_b = st.slider("Lado b", 0.0, 20.0, 5.0)
+    lado_c = st.slider("Lado c", 0.0, 20.0, 5.0)
+# calculo del area
+    area = 0.5 * base * altura
+# calculo del perimetro
+    perimetro = lado_a + lado_b + lado_c
+#resultados
+    st.metric("Área", f"{area:.2f}")
+    st.metric("Perímetro", f"{perimetro:.2f}")
+    st.success("¡Resultados!")
+
+# Rectángulo
+elif figura == "Rectángulo":
+    base = st.slider("Selecciona la base", 0.0, 20.0, 5.0)
+    altura = st.slider("Selecciona la altura", 0.0, 20.0, 5.0)
+# calculo del area
+    area = base * altura
+# calculo del perimetro
+    perimetro = 2 * (base + altura)
+# calculos
+    st.metric("Área", f"{area:.2f}")
+    st.metric("Perímetro", f"{perimetro:.2f}")
+    st.success("¡Resultados!")
+
+# Cuadrado
+elif figura == "Cuadrado":
+    lado = st.slider("Selecciona el lado", 0.0, 20.0, 5.0)
+# calculo del area
+    area = lado**2
+# calculo del perimetro
+    perimetro = 4 * lado
+# resultados
+    st.metric("Área", f"{area:.2f}")
+    st.metric("Perímetro", f"{perimetro:.2f}")
+    st.success("¡Resultados!")
+
+
